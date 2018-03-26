747a1abb624338744388a512c9388d5865b54d43
Petro Kurinnyi <petro.kurinnoy@gmail.com>
2018-03-26 10:22:46 +0300

    Working with @ngrx/store. 322 video

--- ngrx/src/app/app.module.ts
+++ ngrx/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { shoppingListReducer } from './shopping-list/store/shopping-list.reducers';
+import { authReducer } from './auth/store/auth.reducers';
+import { reducers } from './store/app.reducers';
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,7 +23,7 @@ import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer
     ShoppingListModule,
     AuthModule,
     CoreModule,
-    StoreModule.forRoot({ shoppingList: shoppingListReducer })
+    StoreModule.forRoot(reducers)
   ],
   bootstrap: [AppComponent]
 })
